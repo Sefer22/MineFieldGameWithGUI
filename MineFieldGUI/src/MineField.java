e8adb7efcf32335cc6a222e94edda830f4077295
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2023-11-26 23:20:58 +0400

    Add files via upload

--- MineFieldGUI/src/MineField.java
+++ MineFieldGUI/src/MineField.java
@@ -1,12 +1,25 @@
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
 public class MineField {
 	JFrame frame;
+	Btn[][] board = new Btn[10][10];
 	
 	public MineField() {
 		frame = new JFrame("Mine Field");
-		frame.setSize(1000, 1000);
-	
+		frame.setSize(800, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(10,10));
+		
+		for(int row=0; row < board.length; row++) {
+			for(int col=0;col<board[0].length;col++) {
+				Btn b = new Btn(row,col);
+				frame.add(b);
+				board[row][col]=b;
+			}
+		}
+		
 		frame.setVisible(true);
 	}
 	
