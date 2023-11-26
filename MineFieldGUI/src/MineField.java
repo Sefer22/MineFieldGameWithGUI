ab4d7bd52585bc811e4b6898784ab3491058a9c5
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2023-11-26 23:00:50 +0400

    Add files via upload

---
+++ MineFieldGUI/src/MineField.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MineField {
+	JFrame frame;
+	
+	public MineField() {
+		frame = new JFrame("Mine Field");
+		frame.setSize(1000, 1000);
+	
+		frame.setVisible(true);
+	}
+	
+
+}
